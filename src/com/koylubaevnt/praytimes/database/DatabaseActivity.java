6630a01b45ebe824b5d66b3626ee0eecacd104c0
Никита <@programing>
2014-05-05 18:58:41 +0400

    Change-Id: Ia0eb3d6f955cd482c4471a321c3aca73e929a741

--- src/com/koylubaevnt/praytimes/database/DatabaseActivity.java
+++ src/com/koylubaevnt/praytimes/database/DatabaseActivity.java
@@ -1,7 +1,6 @@
 package com.koylubaevnt.praytimes.database;
 
 import com.koylubaevnt.praytimes.R;
-
 import android.app.Activity;
 import android.app.ProgressDialog;
 import android.database.Cursor;
@@ -9,7 +8,8 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.view.Gravity;
 import android.view.View;
-import android.view.ViewGroup.LayoutParams;
+import android.view.View.OnClickListener;
+import android.widget.TableRow.LayoutParams;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TableLayout;
@@ -17,115 +17,117 @@ import android.widget.TableRow;
 import android.widget.TextView;
 
 public class DatabaseActivity extends Activity {
-		 TableLayout table_layout;
-		 EditText firstname_et, lastname_et;
-		 Button addmem_btn;
 
-		 SQLController sqlcon;
+	 TableLayout table_layout;
+	 EditText firstname_et, lastname_et;
+	 Button addmem_btn;
 
-		 ProgressDialog PD;
+	 SQLController sqlcon;
 
-		 @Override
-		 protected void onCreate(Bundle savedInstanceState) {
-		  super.onCreate(savedInstanceState);
-		  setContentView(R.layout.database_activity);
+	 ProgressDialog PD;
 
-		  sqlcon = new SQLController(this);
+	 @Override
+	 protected void onCreate(Bundle savedInstanceState) {
+	  super.onCreate(savedInstanceState);
+	  setContentView(R.layout.database_activity);
 
-		  firstname_et = (EditText) findViewById(R.id.fistname_et_id);
-		  lastname_et = (EditText) findViewById(R.id.lastname_et_id);
-		  addmem_btn = (Button) findViewById(R.id.addmem_btn_id);
-		  table_layout = (TableLayout) findViewById(R.id.tableLayout1);
+	  sqlcon = new SQLController(this);
 
-		  BuildTable();
+	  firstname_et = (EditText) findViewById(R.id.fistname_et_id);
+	  lastname_et = (EditText) findViewById(R.id.lastname_et_id);
+	  addmem_btn = (Button) findViewById(R.id.addmem_btn_id);
+	  table_layout = (TableLayout) findViewById(R.id.tableLayout1);
 
-		  addmem_btn.setOnClickListener(new View.OnClickListener() {
+	  BuildTable();
 
-		   @Override
-		   public void onClick(View v) {
+	  addmem_btn.setOnClickListener(new OnClickListener() {
 
-		    new MyAsync().execute();
+	   @Override
+	   public void onClick(View v) {
 
-		   }
-		  });
+	    new MyAsync().execute();
 
-		 }
+	   }
+	  });
 
-		 private void BuildTable() {
+	 }
 
-		  sqlcon.open();
-		  Cursor c = sqlcon.readEntry();
+	 private void BuildTable() {
 
-		  int rows = c.getCount();
-		  int cols = c.getColumnCount();
+	  sqlcon.open();
+	  Cursor c = sqlcon.readEntry();
 
-		  c.moveToFirst();
+	  int rows = c.getCount();
+	  int cols = c.getColumnCount();
 
-		  // outer for loop
-		  for (int i = 0; i < rows; i++) {
+	  c.moveToFirst();
 
-		   TableRow row = new TableRow(this);
-		   row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
-		     LayoutParams.WRAP_CONTENT));
+	  // outer for loop
+	  for (int i = 0; i < rows; i++) {
 
-		   // inner for loop
-		   for (int j = 0; j < cols; j++) {
+	   TableRow row = new TableRow(this);
+	   row.setLayoutParams(new LayoutParams(LayoutParams.MATCH_PARENT,
+	     LayoutParams.WRAP_CONTENT));
 
-		    TextView tv = new TextView(this);
-		    tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
-		      LayoutParams.WRAP_CONTENT));
-		    tv.setBackgroundResource(R.drawable.ic_launcher);
-		    tv.setGravity(Gravity.CENTER);
-		    tv.setTextSize(18);
-		    tv.setPadding(0, 5, 0, 5);
+	   // inner for loop
+	   for (int j = 0; j < cols; j++) {
 
-		    tv.setText(c.getString(j));
+	    TextView tv = new TextView(this);
+	    tv.setLayoutParams(new LayoutParams(LayoutParams.WRAP_CONTENT,
+	      LayoutParams.WRAP_CONTENT));
+	    //tv.setBackgroundResource(R.drawable.ic_launcher);
+	    tv.setGravity(Gravity.CENTER);
+	    tv.setTextSize(18);
+	    tv.setPadding(0, 5, 0, 5);
 
-		    row.addView(tv);
+	    tv.setText(c.getString(j));
 
-		   }
+	    row.addView(tv);
 
-		   c.moveToNext();
+	   }
 
-		   table_layout.addView(row);
+	   c.moveToNext();
 
-		  }
-		  sqlcon.close();
-		 }
+	   table_layout.addView(row);
 
-		 private class MyAsync extends AsyncTask<Void, Void, Void> {
+	  }
+	  sqlcon.close();
+	 }
 
-		  @Override
-		  protected void onPreExecute() {
-		   super.onPreExecute();
+	 private class MyAsync extends AsyncTask<Void, Void, Void> {
 
-		   table_layout.removeAllViews();
+	  @Override
+	  protected void onPreExecute() {
+	   // TODO Auto-generated method stub
+	   super.onPreExecute();
 
-		   PD = new ProgressDialog(DatabaseActivity.this);
-		   PD.setTitle("Please Wait..");
-		   PD.setMessage("Loading...");
-		   PD.setCancelable(false);
-		   PD.show();
-		  }
+	   table_layout.removeAllViews();
 
-		  @Override
-		  protected Void doInBackground(Void... params) {
+	   PD = new ProgressDialog(DatabaseActivity.this);
+	   PD.setTitle("Please Wait..");
+	   PD.setMessage("Loading...");
+	   PD.setCancelable(false);
+	   PD.show();
+	  }
 
-		   String firstname = firstname_et.getText().toString();
-		   String lastname = lastname_et.getText().toString();
+	  @Override
+	  protected Void doInBackground(Void... params) {
 
-		   // inserting data
-		   sqlcon.open();
-		   sqlcon.insertData(firstname, lastname);
-		   // BuildTable();
-		   return null;
-		  }
+	   String firstname = firstname_et.getText().toString();
+	   String lastname = lastname_et.getText().toString();
 
-		  @Override
-		  protected void onPostExecute(Void result) {
-		   super.onPostExecute(result);
-		   BuildTable();
-		   PD.dismiss();
-		  }
-		 }
+	   // inserting data
+	   sqlcon.open();
+	   sqlcon.insertData(firstname, lastname);
+	   // BuildTable();
+	   return null;
+	  }
+
+	  @Override
+	  protected void onPostExecute(Void result) {
+	   super.onPostExecute(result);
+	   BuildTable();
+	   PD.dismiss();
+	  }
+	 }
 }
--- src/com/koylubaevnt/praytimes/database/SQLController.java
+++ src/com/koylubaevnt/praytimes/database/SQLController.java
@@ -7,38 +7,48 @@ import android.database.SQLException;
 import android.database.sqlite.SQLiteDatabase;
 
 public class SQLController {
-	private DBHelper dbhelper;
-	private Context context;
-	private SQLiteDatabase database;
-	
-	public SQLController(Context c) {
-		context = c;
-	}
-	
-	public SQLController open() throws SQLException {
-		dbhelper = new DBHelper(context);
-		database = dbhelper.getWritableDatabase();
-		return this;
-	}
-	
-	public void close() {
-		dbhelper.close();
-	}
-	
-	public void insertData(String name, String lname) {
-		ContentValues cv = new ContentValues();
-		cv.put(DBHelper.MEMBER_FIRSTNAME, name);
-		cv.put(DBHelper.MEMBER_LASTNAME, lname);
-		database.insert(DBHelper.TABLE_MEMBER, null, cv);
-	}
-	
-	public Cursor readEntry() {
-		String[] allColumns = new String[] { DBHelper.MEMBER_ID, DBHelper.MEMBER_FIRSTNAME, DBHelper.MEMBER_LASTNAME };
-		Cursor c = database.query(DBHelper.TABLE_MEMBER, allColumns, null, null, null, null, null);
-		
-		if (c != null) {
-			c.moveToFirst();
-		}
-		return c;
-	}
+
+ private DBHelper dbhelper;
+ private Context ourcontext;
+ private SQLiteDatabase database;
+
+ public SQLController(Context c) {
+  ourcontext = c;
+ }
+
+ public SQLController open() throws SQLException {
+  dbhelper = new DBHelper(ourcontext);
+  database = dbhelper.getWritableDatabase();
+  return this;
+
+ }
+
+ public void close() {
+  dbhelper.close();
+ }
+
+ public void insertData(String name, String lname) {
+  // TODO Auto-generated method stub
+  ContentValues cv = new ContentValues();
+  cv.put(DBHelper.MEMBER_FIRSTNAME, name);
+  cv.put(DBHelper.MEMBER_LASTNAME, lname);
+  database.insert(DBHelper.TABLE_MEMBER, null, cv);
+
+ }
+
+ public Cursor readEntry() {
+  // TODO Auto-generated method stub
+  String[] allColumns = new String[] { DBHelper.MEMBER_ID, DBHelper.MEMBER_FIRSTNAME,
+		  DBHelper.MEMBER_LASTNAME };
+
+  Cursor c = database.query(DBHelper.TABLE_MEMBER, allColumns, null, null, null,
+    null, null);
+
+  if (c != null) {
+   c.moveToFirst();
+  }
+  return c;
+
+ }
+
 }
